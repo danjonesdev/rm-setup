d631f514a5844382c0c88ffed028bce8d2197e7b
danjonesdev <dannyjones360@gmail.com>
2017-10-06 16:21:49 +0100

    comment reducer + action types

--- src/types/index.js
+++ src/types/index.js
@@ -16,23 +16,23 @@ import type { Store as ReduxStore } from 'redux';
 //   },
 // };
 
-export type Reducer = {
-  home: Home,
-  userInfo: UserInfo,
-  router: any,
-};
+// export type Reducer = {
+//   home: Home,
+//   userInfo: UserInfo,
+//   router: any,
+// };
+//
+// export type Action =
+//   { type: 'USERS_REQUESTING' } |
+//   { type: 'USERS_SUCCESS', data: Array<Object> } |
+//   { type: 'USERS_FAILURE', err: any } |
+//   { type: 'USER_REQUESTING', userId: string } |
+//   { type: 'USER_SUCCESS', userId: string, data: Object } |
+//   { type: 'USER_FAILURE', userId: string, err: any };
 
-export type Action =
-  { type: 'USERS_REQUESTING' } |
-  { type: 'USERS_SUCCESS', data: Array<Object> } |
-  { type: 'USERS_FAILURE', err: any } |
-  { type: 'USER_REQUESTING', userId: string } |
-  { type: 'USER_SUCCESS', userId: string, data: Object } |
-  { type: 'USER_FAILURE', userId: string, err: any };
-
-export type Store = ReduxStore<Reducer, Action>;
+export type Store = ReduxStore;
 // eslint-disable-next-line no-use-before-define
-export type Dispatch = (action: Action | ThunkAction | PromiseAction | Array<Action>) => any;
+export type Dispatch = (action | ThunkAction | PromiseAction | Array) => any;
 export type GetState = () => Object;
-export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
-export type PromiseAction = Promise<Action>;
+export type ThunkAction = (dispatch, getState) => any;
+export type PromiseAction = Promise;
