6d331902815b2ace5330e1312003a101209ea42b
dan jones <dannyjones360@gmail.com>
2017-11-05 18:43:33 +0000

    add svg classes

--- src/containers/App/Header/index.js
+++ src/containers/App/Header/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import type { Element } from 'react';
 import config from '../../../config';
 
-import { Fb } from '../../../components/Elements/Svg';
+import { Fb, Tw, Insta, Sc } from '../../../components/Elements/Svg';
 
 const Header = (): Element<'div'> => (
   <div>
@@ -12,6 +12,9 @@ const Header = (): Element<'div'> => (
       <img className="h3" src={require('../assets/logo.svg')} alt="Logo" role="presentation" />
       <h1>{config.app.title}</h1>
       <Fb />
+      <Tw />
+      <Insta />
+      <Sc />
     </header>
   </div>
 );
