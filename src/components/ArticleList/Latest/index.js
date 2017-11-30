9d8ecc85726f3486f615387a6988d61448fde854
dan jones <dannyjones360@gmail.com>
2017-11-30 14:05:48 +0000

    ammends to cards and add logo

--- src/components/ArticleList/Latest/index.js
+++ src/components/ArticleList/Latest/index.js
@@ -4,9 +4,6 @@ import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import Heading from '../../Elements/Heading';
-// import GetImage from '../../Helpers/GetImage';
-
 // Export this for unit testing more easily
 export class LatestArticleList extends PureComponent {
   render() {
@@ -15,14 +12,14 @@ export class LatestArticleList extends PureComponent {
         <div className="row">
           {this.props.list.map((article, i) => (
             <Link className="link" to={`/Article/${article._id}`}>
-              <article key={i} className="col-sm-6  pv3">
+              <article key={i} className="col-sm-6  pv3  latestArticleList__col">
                 <figure className="rel">
                   <span className="abs  pv1  ph2  t8  white latestArticleList__label">Latest</span>
                   <img className="mb3  latestArticleList__img" alt="" src={article.img} />
                 </figure>
                 <span className="grey  t8">10/17/09</span>
-                <Heading type="h2" color="black" text={article.title} />
-                <p className="t8  lh-copy  font-normal-a  dark-grey  latestArticleList__intro">{article.body['0'].section.content}</p>
+                <h2 className="t7  pv2  black  latestArticleList__title">{article.title}</h2>
+                <p className="t8  font-normal-a  dark-grey  latestArticleList__intro">{article.description}</p>
               </article>
             </Link>
           ))}
