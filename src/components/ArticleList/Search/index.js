9ee18f9cd449ee9d2899f649c0d5459698f433e8
dan jones <dannyjones360@gmail.com>
2017-12-16 22:37:15 +0000

    add link class

--- src/components/ArticleList/Search/index.js
+++ src/components/ArticleList/Search/index.js
@@ -62,7 +62,7 @@ export class SearchArticleList extends PureComponent {
                   </div>
                   <div className="col-sm-12">
                     <span className="grey  t8"><time dateTime="10/17/09">10/17/09</time> | </span>
-                    <span className="grey  t8  cp" onClick={() => this.handleClick(article.author, 'author')}>{article.author}</span>
+                    <span className="grey  t8  cp  link" onClick={() => this.handleClick(article.author, 'author')}>{article.author}</span>
                     <h2 onClick={() => this.handleClick(article.title, 'article')} className="black  t7  pt2  cp  latestArticleList__title">{article.title}</h2>
                     <p className="grey  t8  pv2  latestArticleList__intro">{article.description}</p>
                   </div>
