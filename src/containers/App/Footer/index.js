35b39e01180902344854471c81fd37ac28b814d4
dan jones <dannyjones360@gmail.com>
2018-01-31 13:32:11 +0000

    ammends to footer

--- src/containers/App/Footer/index.js
+++ src/containers/App/Footer/index.js
@@ -3,6 +3,7 @@
 
 import React, { PureComponent } from 'react';
 import Instafeed from 'react-instafeed';
+import { Link } from 'react-router-dom';
 
 export class Footer extends PureComponent {
   shouldComponentUpdate() {
@@ -10,27 +11,44 @@ export class Footer extends PureComponent {
   }
   render() {
     return (
-      <footer className="container-fluid  rel  bt  bw1  z9">
-        <div className="row">
-          <div className="col-sm-12">
-            test
-          </div>
-          <div className="col-sm-12">
-            <div id="instaFeed" className="container  mv3">
-              <Instafeed
-                limit="3"
-                resolution="standard_resolution"
-                sortBy="most-recent"
-                target="instaFeed"
-                template=""
-                userId="6646546960"
-                clientId="1375f42790124e1ba319055cd4f93ec5"
-                accessToken="6646546960.1677ed0.8eb1d2cceba64706aed91890d352c904"
-              />
+      <div className="rel  bt  bw1  z9  pv2  footer">
+        <footer className="container  footer">
+          <div className="row">
+            <div className="col-md-12">
+              <ul className="pa0  ma0  footer__ul">
+                <li>
+                  <Link className="title-font  black  dib  ph2  t5  pt2  link" to={'/'}>Home</Link>
+                </li>
+                <li>
+                  <Link className="title-font  black  dib  ph2  t5  pt2  link" to={'/Authors'}>Authors</Link>
+                </li>
+                <li>
+                  <Link className="title-font  black  dib  ph2  t5  pt2  link" to={'/About'}>About Us</Link>
+                </li>
+                <li>
+                  <Link className="title-font  black  dib  ph2  t5  pt2  link" to={'/Contact'}>Contact</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-12  ph6-md  mv2-md">
+              <div className="row">
+                <div id="instaFeed" className="container">
+                  <Instafeed
+                    limit="3"
+                    resolution="standard_resolution"
+                    sortBy="most-recent"
+                    target="instaFeed"
+                    template=""
+                    userId="6646546960"
+                    clientId="1375f42790124e1ba319055cd4f93ec5"
+                    accessToken="6646546960.1677ed0.8eb1d2cceba64706aed91890d352c904"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </footer>
+        </footer>
+      </div>
     );
   }
 }
