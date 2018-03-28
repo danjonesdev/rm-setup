92854076f3cc698edaa5f328ea4547b4118a1c8a
dan jones <dannyjones360@gmail.com>
2018-03-29 00:09:46 +0100

    ammends to contact pages

--- src/containers/Pages/About/index.js
+++ src/containers/Pages/About/index.js
@@ -2,7 +2,6 @@
 
 import React, { PureComponent } from 'react';
 import Helmet from 'react-helmet';
-import { Link } from 'react-router-dom';
 
 export class About extends PureComponent {
   componentDidMount() {
@@ -18,39 +17,8 @@ export class About extends PureComponent {
         <Helmet title="About" />
         <div className="container  mb4">
           <div className="row">
-            <div className="col-sm-18  col-sm-offset-3">
-              <h1 className="mt4  mb3">About Rendah</h1>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-18  col-sm-offset-3">
-              <h2 className="pv2">Why?</h2>
-              <p className="pv3">
-                It didn&#39;t seem right that whilst so much was happening within the scene, there were so few places to read about it.
-                Rendah was created to provide some webspace exposure for halftime/beats producers and DJ&#39;s, we honestly just wanted
-                somewhere for people to read about the hype, so here it is.
-              </p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-18  col-sm-offset-3">
-              <h2 className="pv2">Who?</h2>
-              <p className="pv3">
-                Web developer/Halftime beats Producer <a href="www.github.com" rel="noopener noreferrer" target="_blank" className="underline  black  link">Dan Jones</a>
-                (AKA <a href="www.github.com" rel="noopener noreferrer" target="_blank" className="underline  black link">Aagentah</a>) founded & created Rendah as a keen
-                side project using Node.js & React.js, Rendah is completely open-sourced and we encourage other developers to
-                help <a href="https://github.com/danjonesdev" rel="noopener noreferrer" target="_blank" className="underline  black  link">contribute to the site!</a>
-              </p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-18  col-sm-offset-3">
-              <h2 className="pv2">What About You?</h2>
-              <p className="pv3">
-                We&#39;re constantly encouraging people to get involved, as clichue as it sounds, Rendah is about community, so wether you&#39;re an
-                artist, a designer, a writer, a developer or anything else and want to get
-                involed <Link to={'/Contact'} className="underline  black  link">please get in touch!</Link>
-              </p>
+            <div className="col-sm-24">
+              <h1 className="black  title-font  t5  pt5  pv4">About Rendah</h1>
             </div>
           </div>
         </div>
--- src/containers/Pages/Contact/index.js
+++ src/containers/Pages/Contact/index.js
@@ -21,15 +21,15 @@ export class Contact extends PureComponent {
           <div className="row">
             <div className="col-xs-14">
               <h1 className="black  title-font  t5  pt5  pv4">Contact</h1>
-              <p>Get involved. We&apos;re constantly on the look out for new members of the Rendah team.</p>
+              <p>Get involved. We&apos;re constantly on the look out for new members for the Rendah team.</p>
               <ul>
                 <li className="pv1">Are you a listener and want to start writing articles? - <strong>Get involved</strong></li>
-                <li className="pv1">Are you a developer and want to help us code the site/side projects?- <strong>Get involved</strong></li>
-                <li className="pv1">Are you an artist and want a Rendah feature/review/interview?- <strong>Get involved</strong></li>
-                <li className="pv1">Are you a visual artist and want to promote artwork on the site?- <strong>Get involved</strong></li>
+                <li className="pv1">Are you a developer and want to help us code the site/side projects? - <strong>Get involved</strong></li>
+                <li className="pv1">Are you an artist and want a Rendah feature/review/interview? - <strong>Get involved</strong></li>
+                <li className="pv1">Are you a visual artist and want to promote artwork on the site? - <strong>Get involved</strong></li>
               </ul>
-              <p>Even if you&apos;re none of the above and want to get involved, we encourage you to get in touch!</p>
-              <p>Please email <a href="mailto:info@rendahmag.com" target="_blank" rel="noopener noreferrer">info@rendahmag.com</a>.</p>
+              <p>Even if you&apos;re none of the above and want to get involved, we encourage you to get in touch!<br /><br /></p>
+              <p>Please email: <a href="mailto:info@rendahmag.com" target="_blank" rel="noopener noreferrer">info@rendahmag.com</a></p>
             </div>
             <div className="col-xs-10">
               <div className="container-fluid  pv4">
